dc8d6acf70e6e1d2645b348036156170b8d71e99
Iyas01 <casicabal@gmail.com>
2018-10-03 15:31:25 +0800

    Task 7

---
+++ app/src/main/java/com/example/cbcabal/roomwordssample/Word.java
@@ -0,0 +1,21 @@
+package com.example.cbcabal.roomwordssample;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "word_table")
+public class Word {
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    // Constructor
+    public Word(@NonNull String word) { this.mWord = word;}
+
+    public String getmWord() {
+        return mWord;
+    }
+}
